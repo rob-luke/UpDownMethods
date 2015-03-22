bea3eeae70ad50afaa536b4892074da5a461a6fa
Robert Luke <codles@users.noreply.github.com>
2015-03-22 11:03:11 +0100

    Getting travis to work

--- tests/Levitt1971-Fig5.py
+++ tests/Levitt1971-Fig5.py
@@ -1,28 +1,10 @@
 from UpDownMethods import CORRECT, INCORRECT
 import UpDownMethods as ud
 import numpy as np
+import matplotlib as mpl     # This is just so it works on 
+mpl.use('Agg')               # travis-ci.org
 import matplotlib.pyplot as plt
 import unittest
-
-import os
-
-
-print("Path at terminal when executing this file")
-print(os.getcwd() + "\n")
-
-print("This file path, relative to os.getcwd()")
-print(__file__ + "\n")
-
-print("This file full path (following symlinks)")
-full_path = os.path.realpath(__file__)
-print(full_path + "\n")
-
-print("This file directory and name")
-path, file = os.path.split(full_path)
-print(path + ' --> ' + file + "\n")
-
-print("This file directory only")
-print(os.path.dirname(full_path))
 
 
 #
