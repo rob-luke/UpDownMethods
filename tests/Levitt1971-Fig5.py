94052dd198e512fde32395e15ac3ba12cef40415
Robert Luke <codles@users.noreply.github.com>
2015-03-22 10:17:25 +0100

    Getting travis to work

--- tests/Levitt1971-Fig5.py
+++ tests/Levitt1971-Fig5.py
@@ -4,6 +4,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import unittest
 
+import os
+
+
+print("Path at terminal when executing this file")
+print(os.getcwd() + "\n")
+
+print("This file path, relative to os.getcwd()")
+print(__file__ + "\n")
+
+print("This file full path (following symlinks)")
+full_path = os.path.realpath(__file__)
+print(full_path + "\n")
+
+print("This file directory and name")
+path, file = os.path.split(full_path)
+print(path + ' --> ' + file + "\n")
+
+print("This file directory only")
+print(os.path.dirname(full_path))
+
+
 #
 # Simulation parameters
 #
